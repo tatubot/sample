f8f32ac5a790d62a57d8fdb391bb153b32ca84f9
tatubot <tatu0308@outlook.com>
2023-12-15 15:01:30 +0900

    12/15 てすとだよぉ

--- js/animation.js
+++ js/animation.js
@@ -1,14 +1,35 @@
 //scroll_effect
-$(window).scroll(function () {
-    var scrollAnimationElm = document.querySelectorAll('.scroll_up ,.scroll_left , .scroll_right');
-    var scrollAnimationFunc = function () {
-      for (var i = 0; i < scrollAnimationElm.length; i++) {
-        var triggerMargin = 100;
-        if (window.innerHeight > scrollAnimationElm[i].getBoundingClientRect().top + triggerMargin) {
-          scrollAnimationElm[i].classList.add('on');
+  if(window.matchMedia('(max-width:767px)').matches){
+    //スマホ
+    $(window).on('load', function(){
+
+    })
+    $(window).scroll(function () {
+      var scrollAnimationElm = document.querySelectorAll('.scroll_up');
+      var scrollAnimationFunc = function () {
+        for (var i = 0; i < scrollAnimationElm.length; i++) {
+          var triggerMargin = 100;
+          if (window.innerHeight > scrollAnimationElm[i].getBoundingClientRect().top + triggerMargin) {
+            scrollAnimationElm[i].classList.add('on');
+          }
         }
       }
-    }
-    window.addEventListener('load', scrollAnimationFunc);
-    window.addEventListener('scroll', scrollAnimationFunc);
-  });
+      window.addEventListener('load', scrollAnimationFunc);
+      window.addEventListener('scroll', scrollAnimationFunc);
+    });
+  }else if(window.matchMedia('(min-width:768px)').matches){
+    //パソコン
+    $(window).scroll(function () {
+      var scrollAnimationElm = document.querySelectorAll('.scroll_up ,.scroll_left , .scroll_right');
+      var scrollAnimationFunc = function () {
+        for (var i = 0; i < scrollAnimationElm.length; i++) {
+          var triggerMargin = 100;
+          if (window.innerHeight > scrollAnimationElm[i].getBoundingClientRect().top + triggerMargin) {
+            scrollAnimationElm[i].classList.add('on');
+          }
+        }
+      }
+      window.addEventListener('load', scrollAnimationFunc);
+      window.addEventListener('scroll', scrollAnimationFunc);
+    });
+  }
